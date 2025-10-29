16316f94f212e37728d89231d05e306d0ad36790
Mad Triathlete <h_bicakci@msn.com>
2025-10-29 08:28:11 -0400

    test groups added

---
+++ tests/day02/test-groups-intro.spec.js
@@ -0,0 +1,15 @@
+import { test } from "@playwright/test";
+
+test.describe("", () => {
+  test("Test Case 1", async () => {
+    console.log("Test Case 1 is executed");
+  });
+
+  test("Test Case 2", async () => {
+    "Test Case 2 is executed";
+  });
+
+  test("Test Case 3", async () => {
+    "Test Case 3 is executed";
+  });
+});
